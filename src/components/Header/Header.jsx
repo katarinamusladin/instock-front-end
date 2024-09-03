e450bf13635158081d51738ae5735c16578f5421
Katarina Musladin <katarinamcao@gmail.com>
2024-09-03 16:28:22 -0400

    Set up my style folder with partials

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import './Header.scss';
-
+import Logo from '../../assets/images/Logo/InStock-Logo.svg';
 
 function Header(){
   return (
-    
+    <div className="navbar">
+    <div className="navbar__container">
+      <div className="navbar__logo-container">
+        <div className="navbar__link">
+          <img src={Logo} className="navbar__img" alt="Instock" />
+        </div>
+      </div>
+      <div className="navbar__links">
+        <ul className="navbar__list">
+          <li className="navbar__link navbar__warehouses">Warehouses</li>
+          <li className="navbar__link navbar__inventory">Inventory</li>
+        </ul>
+      </div>
+    </div>
+  </div>
   );
 }
 
