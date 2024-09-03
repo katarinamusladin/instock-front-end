0fb2f7fef1494aabc92c847dd67b70724b763667
Katarina Musladin <katarinamcao@gmail.com>
2024-09-03 19:14:18 -0400

    Created Header section, Have to add Link and Hover active state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,32 @@
-import './Header.scss';
-import Logo from '../../assets/images/Logo/InStock-Logo.svg';
+import "./Header.scss";
+import Logo from "../../assets/images/Logo/InStock-Logo.svg";
+import { Link } from "react-router-dom";
 
-function Header(){
+function Header() {
   return (
     <div className="navbar">
-    <div className="navbar__container">
-      <div className="navbar__logo-container">
-        <div className="navbar__link">
-          <img src={Logo} className="navbar__img" alt="Instock" />
+      <div className="navbar__container">
+        <div className="navbar__logo-container">
+          <a href="#">
+            <img src={Logo} className="navbar__img" alt="Instock" />
+          </a>
+        </div>
+        <div className="navbar__links">
+          <ul className="navbar__list">
+            <li>
+              <a href="#" className="navbar__link navbar__warehouses">
+                Warehouses
+              </a>
+            </li>
+            <li>
+              <a href="#" className="navbar__link navbar__inventory">
+                Inventory
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div className="navbar__links">
-        <ul className="navbar__list">
-          <li className="navbar__link navbar__warehouses">Warehouses</li>
-          <li className="navbar__link navbar__inventory">Inventory</li>
-        </ul>
-      </div>
     </div>
-  </div>
   );
 }
 
