d4f673b29c0c73e0eb4aff7f8075c0ba00308515
Jessica Hua <jhua@live.ca>
2024-09-03 21:39:30 -0400

    browser router setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import './App.scss'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import "./App.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
 
 function App() {
-
-
-  return (
-    <>
-      <Header />
-      <Footer />
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/home" element={<Navigate to="/" />} />
+					<Route path="/notfound" element={<NotFoundPage />} />
+					<Route path="*" element={<Navigate to="/notfound" />} />
+					<Header />
+					<Footer />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
