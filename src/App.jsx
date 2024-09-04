6f723735fc2558d2a41f026b496f75c7ae0b5564
Jessica Hua <jhua@live.ca>
2024-09-03 22:08:57 -0400

    update with more routes.

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,32 @@ function App() {
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/home" element={<Navigate to="/" />} />
+					<Route path="/warehouses" element={<Navigate to="/" />} />
+					<Route
+						path="/warehouses/:warehouseId"
+						element={<WarehouseDetailsPage />}
+					/>
+					<Route
+						path="/warehouses/:warehouseId/edit"
+						element={<WarehouseEditPage />}
+					/>
+					<Route
+						path="/warehouses/:warehouseId/add"
+						element={<WarehouseAddPage />}
+					/>
+					<Route path="/inventory" element={<InventoryPage />} />
+					<Route
+						path="/inventory/:inventoryId"
+						element={<InventoryItemPage />}
+					/>
+					<Route
+						path="/inventory/:inventoryId/edit"
+						element={<InventoryEditPage />}
+					/>
+					<Route
+						path="/inventory/:inventoryId/add"
+						element={<InventorAddPage />}
+					/>
 					<Route path="/notfound" element={<NotFoundPage />} />
 					<Route path="*" element={<Navigate to="/notfound" />} />
 					<Header />
