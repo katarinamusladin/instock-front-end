8e1bc51e577ef97e5f0f268e72497435ff54f398
Katarina Musladin <katarinamcao@gmail.com>
2024-09-05 12:29:12 -0400

    Added icons on the right side of my WareHouseTitle

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import deleteIcon from "../../assets/images/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/images/icons/edit-24px.svg";
 import Arrows from "../../assets/images/icons/sort-24px.svg";
+import arrowRight from "../../assets/images/icons/chevron_right-24px.svg";
 import "./WarehouseList.scss";
 import { Link } from "react-router-dom";
 import WarehouseHeaderList from "../WarehouseHeaderList/WarehouseHeaderList";
@@ -11,9 +12,9 @@ function WarehouseList() {
 
   useEffect(() => {
     const baseUrl = import.meta.env.VITE_BASE_URL;
-  const port = import.meta.env.VITE_PORT;
-  const endpoint = import.meta.env.VITE_BASE_ENDPOINT;
-  const apiUrl = `${baseUrl}:${port}/${endpoint}`;
+    const port = import.meta.env.VITE_PORT;
+    const endpoint = import.meta.env.VITE_BASE_ENDPOINT;
+    const apiUrl = `${baseUrl}:${port}/${endpoint}`;
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => setWarehouses(data))
@@ -47,48 +48,29 @@ function WarehouseList() {
           <div className="warehouses__text-box">
             <div className="warehouses__column">
               <div className="warehouses__content warehouses__content--short">
-                <h3 className="warehouses__mobile-header">
-                  Warehouse
-                  <img
-                    src={Arrows}
-                    alt="Arrows"
-                    className="warehouses__mobile-header--icon"
-                  />
-                </h3>
+                <h3 className="warehouses__mobile-header">Warehouse</h3>
+                <div className="warehouses__name-wrapper">
                 <p className="warehouses__name">{warehouse.warehouse_name}</p>
+                <img
+                  src={arrowRight}
+                  alt="Arrow Right"
+                  className="warehouses__name--icon"
+                />
+                </div>
               </div>
               <div className="warehouses__content warehouses__content--long">
-                <h3 className="warehouses__mobile-header">
-                  Address
-                  <img
-                    src={Arrows}
-                    alt="Arrows"
-                    className="warehouses__mobile-header--icon"
-                  />
-                </h3>
+                <h3 className="warehouses__mobile-header">Address</h3>
                 <p>{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</p>
               </div>
             </div>
             <div className="warehouses__column">
               <div className="warehouses__content warehouses__content--short">
-                <h3 className="warehouses__mobile-header">
-                  Contact Name
-                  <img
-                    src={Arrows}
-                    alt="Arrows"
-                    className="warehouses__mobile-header--icon"
-                  />
-                </h3>
+                <h3 className="warehouses__mobile-header">Contact Name</h3>
                 <p>{warehouse.contact_name}</p>
               </div>
               <div className="warehouses__content warehouses__content--long">
                 <h3 className="warehouses__mobile-header">
                   Contact Information
-                  <img
-                    src={Arrows}
-                    alt="Arrows"
-                    className="warehouses__mobile-header--icon"
-                  />
                 </h3>
                 <p>{warehouse.contact_phone}</p>
                 <p>{warehouse.contact_email}</p>
