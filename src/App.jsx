8d0323555433c806802f5e047f80cf9534115258
Jessica Hua <jhua@live.ca>
2024-09-07 13:58:30 -0400

    design for edit warehouse mobile.

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,44 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage.jsx";
+import InventoryPage from "./pages/InventoryPage/InventoryPage.jsx";
 import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx";
 import InventoryDetailsPage from "./pages/InventoryDetailsPage/InventoryDetailsPage.jsx";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
+// import AddWarehousePage from "./pages/AddWarehousePage/AddWarehousePage.jsx";
 import AddInventoryPage from "./pages/AddInventoryPage/AddInventoryPage.jsx";
-
+import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage.jsx";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
 import WarehouseList from "./components/WarehouseList/WarehouseList.jsx";
-import InventoryPage from "./pages/InventoryPage/InventoryPage.jsx";
+
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<Navigate to="/" />} />
-          {/* <Route path="/warehouses" element={<Navigate to="/" />} /> */}
-          <Route path="/warehouses" element={<WarehouseList />} />
-          <Route
-            path="/warehouses/:warehouseId"
-            element={<WarehouseDetailsPage />}
-          />
-
-          <Route
-            path="/inventories/:inventoryId"
-            element={<InventoryDetailsPage />}
-          />
-
-          
-          {/* need to figure out what route to use */}
-          <Route
-            path="/inventories/add"
-            element={<AddInventoryPage />}
-          />
-
-          <Route path="/inventory" element={<InventoryPage />} />
-
-          {/* <Route
-						path="/warehouses/:warehouseId/edit"
-						element={<WarehouseEditPage />}
+	return (
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/home" element={<Navigate to="/" />} />
+					{/* <Route path="/warehouses" element={<Navigate to="/" />} /> */}
+					<Route path="/warehouses" element={<WarehouseList />} />
+					<Route
+						path="/warehouses/:warehouseId"
+						element={<WarehouseDetailsPage />}
 					/>
 					<Route
+						path="/warehouses/edit/:warehouseId"
+						element={<EditWarehousePage />}
+					/>
+					<Route
+						path="/inventories/:inventoryId"
+						element={<InventoryDetailsPage />}
+					/>
+
+					{/* need to figure out what route to use */}
+					<Route path="/inventories/add" element={<AddInventoryPage />} />
+
+					<Route path="/inventory" element={<InventoryPage />} />
+
+					{/*<Route
 						path="/warehouses/:warehouseId/add"
 						element={<WarehouseAddPage />}
 					/>
@@ -55,13 +52,13 @@ function App() {
 						path="/inventory/:inventoryId/add"
 						element={<InventorAddPage />}
 					/> */}
-          <Route path="/notfound" element={<NotFoundPage />} />
-          <Route path="*" element={<Navigate to="/notfound" />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
+					<Route path="/notfound" element={<NotFoundPage />} />
+					<Route path="*" element={<Navigate to="/notfound" />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
