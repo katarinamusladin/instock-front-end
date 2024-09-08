7f85c5f578ede7aaf90aff1c135d1c3e40ec3bdd
Jessica Hua <jhua@live.ca>
2024-09-08 18:22:26 -0400

    fixed routes on front end and everything should now be linked. started TODO list in README file.

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,11 @@ import HomePage from "./pages/HomePage/HomePage.jsx";
 import InventoryPage from "./pages/InventoryPage/InventoryPage.jsx";
 import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx";
 import InventoryDetailsPage from "./pages/InventoryDetailsPage/InventoryDetailsPage.jsx";
-// import AddWarehousePage from "./pages/AddWarehousePage/AddWarehousePage.jsx";
+import AddWarehousePage from "./pages/AddWarehousePage/AddWarehousePage.jsx";
 import AddInventoryPage from "./pages/AddInventoryPage/AddInventoryPage.jsx";
 import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage.jsx";
 import EditInventoryPage from "./pages/EditInventoryPage/EditInventoryPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
-import WarehouseList from "./components/WarehouseList/WarehouseList.jsx";
 
 function App() {
 	return (
@@ -21,37 +20,26 @@ function App() {
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/home" element={<Navigate to="/" />} />
-					{/* <Route path="/warehouses" element={<Navigate to="/" />} /> */}
-					<Route path="/warehouses" element={<WarehouseList />} />
+					<Route path="/warehouses" element={<Navigate to="/" />} />
+					<Route path="/inventories" element={<InventoryPage />} />
 					<Route
 						path="/warehouses/:warehouseId"
 						element={<WarehouseDetailsPage />}
-					/>
-					<Route
-						path="/warehouses/:warehouseId/edit"
-						element={<EditWarehousePage />}
 					/>
 					<Route
 						path="/inventories/:inventoryId"
 						element={<InventoryDetailsPage />}
 					/>
 					<Route
+						path="/warehouses/:warehouseId/edit"
+						element={<EditWarehousePage />}
+					/>
+					<Route
 						path="/inventories/:inventoryId/edit"
 						element={<EditInventoryPage />}
 					/>
-					{/* need to figure out what route to use */}
+					<Route path="/warehouses/add" element={<AddWarehousePage />} />
 					<Route path="/inventories/add" element={<AddInventoryPage />} />
-
-					<Route path="/inventory" element={<InventoryPage />} />
-
-					{/*<Route
-						path="/warehouses/:warehouseId/add"
-						element={<WarehouseAddPage />}
-					/>
-					<Route
-						path="/inventory/:inventoryId/add"
-						element={<InventorAddPage />}
-					/> */}
 					<Route path="/notfound" element={<NotFoundPage />} />
 					<Route path="*" element={<Navigate to="/notfound" />} />
 				</Routes>
--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -4,142 +4,148 @@ import editIcon from "../../assets/images/icons/edit-24px.svg";
 import Arrows from "../../assets/images/icons/sort-24px.svg";
 import arrowRight from "../../assets/images/icons/chevron_right-24px.svg";
 import "./WarehouseList.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import WarehouseHeaderList from "../WarehouseHeaderList/WarehouseHeaderList";
 import SearchHeader from "../SearchHeader/SearchHeader";
 import DeleteModal from "../DeleteModal/DeleteModal";
 
 function WarehouseList() {
-  document.title = "Warehouses";
-  const [warehouses, setWarehouses] = useState([]);
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [selectedWarehouse, setSelectedWarehouse] = useState(null);
-  const columnsData = [
-    ["Warehouse", "Address"],
-    ["Contact Name", "Contact Information"],
-  ];
+	document.title = "Warehouses";
+	const [warehouses, setWarehouses] = useState([]);
+	const [showDeleteModal, setShowDeleteModal] = useState(false);
+	const [selectedWarehouse, setSelectedWarehouse] = useState(null);
+	const columnsData = [
+		["Warehouse", "Address"],
+		["Contact Name", "Contact Information"],
+	];
 
-  useEffect(() => {
-    const baseUrl = import.meta.env.VITE_BASE_URL;
-    const port = import.meta.env.VITE_PORT;
-    const endpoint = import.meta.env.VITE_BASE_ENDPOINT;
-    const apiUrl = `${baseUrl}:${port}/${endpoint}`;
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => setWarehouses(data))
-      .catch((error) => console.error("Error fetching warehouses:", error));
-  }, []);
+	useEffect(() => {
+		const baseUrl = import.meta.env.VITE_BASE_URL;
+		const port = import.meta.env.VITE_PORT;
+		const endpoint = import.meta.env.VITE_BASE_ENDPOINT;
+		const apiUrl = `${baseUrl}:${port}/${endpoint}`;
+		fetch(apiUrl)
+			.then((response) => response.json())
+			.then((data) => setWarehouses(data))
+			.catch((error) => console.error("Error fetching warehouses:", error));
+	}, []);
 
-  const handleDelete = (warehouse) => {
-    setSelectedWarehouse(warehouse);
-    setShowDeleteModal(true);
-  };
+	const handleDelete = (warehouse) => {
+		setSelectedWarehouse(warehouse);
+		setShowDeleteModal(true);
+	};
 
-  const handleCloseModal = () => {
-    setShowDeleteModal(false);
-    setSelectedWarehouse(null);
-  };
+	const handleCloseModal = () => {
+		setShowDeleteModal(false);
+		setSelectedWarehouse(null);
+	};
 
-  const confirmDeleteWarehouse = () => {
-    if (!selectedWarehouse) return;
+	const confirmDeleteWarehouse = () => {
+		if (!selectedWarehouse) return;
 
-    const baseUrl = import.meta.env.VITE_BASE_URL;
-    const port = import.meta.env.VITE_PORT;
-    const endpoint = import.meta.env.VITE_BASE_ENDPOINT;
-    const deleteUrl = `${baseUrl}:${port}/${endpoint}/${selectedWarehouse.id}`;
+		const baseUrl = import.meta.env.VITE_BASE_URL;
+		const port = import.meta.env.VITE_PORT;
+		const endpoint = import.meta.env.VITE_BASE_ENDPOINT;
+		const deleteUrl = `${baseUrl}:${port}/${endpoint}/${selectedWarehouse.id}`;
 
-    fetch(deleteUrl, {
-      method: "DELETE",
-    })
-      .then((response) => {
-        if (response.ok) {
-          setWarehouses((prevWarehouses) =>
-            prevWarehouses.filter(
-              (warehouse) => warehouse.id !== selectedWarehouse.id
-            )
-          );
-          handleCloseModal();
-        } else {
-          console.error("Error deleting warehouse:", response);
-        }
-      })
-      .catch((error) => console.error("Error deleting warehouse:", error));
-  };
+		fetch(deleteUrl, {
+			method: "DELETE",
+		})
+			.then((response) => {
+				if (response.ok) {
+					setWarehouses((prevWarehouses) =>
+						prevWarehouses.filter(
+							(warehouse) => warehouse.id !== selectedWarehouse.id
+						)
+					);
+					handleCloseModal();
+				} else {
+					console.error("Error deleting warehouse:", response);
+				}
+			})
+			.catch((error) => console.error("Error deleting warehouse:", error));
+	};
 
-  return (
-    <div className="warehouses">
-      <SearchHeader title="Warehouses" type="Warehouse" addPath="/warehouses/add"/>
-      <WarehouseHeaderList
-        columns={columnsData}
-        actionText="Actions"
-        iconSrc={Arrows}
-      />
+	return (
+		<div className="warehouses">
+			<SearchHeader
+				title="Warehouses"
+				type="Warehouse"
+				addPath="/warehouses/add"
+			/>
+			<WarehouseHeaderList
+				columns={columnsData}
+				actionText="Actions"
+				iconSrc={Arrows}
+			/>
 
-      {warehouses.map((warehouse) => (
-        <div key={warehouse.id} className="warehouses__item">
-          <div className="warehouses__text-box">
-            <div className="warehouses__column">
-              <div className="warehouses__content warehouses__content--long">
-                <h3 className="warehouses__mobile-header">Warehouse</h3>
-                <Link
-                  to={`/warehouses/${warehouse.id}`}
-                  className="warehouses__link"
-                >
-                  <div className="warehouses__name-wrapper">
-                    <p className="warehouses__name">
-                      {warehouse.warehouse_name}
-                    </p>
-                    <img
-                      src={arrowRight}
-                      alt="Arrow Right"
-                      className="warehouses__name--icon"
-                    />
-                  </div>
-                </Link>
-              </div>
-              <div className="warehouses__content warehouses__content--long">
-                <h3 className="warehouses__mobile-header">Address</h3>
-                <p>{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</p>
-              </div>
-            </div>
-            <div className="warehouses__column">
-              <div className="warehouses__content warehouses__content--long">
-                <h3 className="warehouses__mobile-header">Contact Name</h3>
-                <p>{warehouse.contact_name}</p>
-              </div>
-              <div className="warehouses__content warehouses__content--long">
-                <h3 className="warehouses__mobile-header">
-                  Contact Information
-                </h3>
-                <p>{warehouse.contact_phone}</p>
-                <p>{warehouse.contact_email}</p>
-              </div>
-            </div>
-          </div>
-          <div className="warehouses__action">
-            <h3 className="warehouses__mobile-header active">ACTIONS</h3>
-            <button
-              className="warehouses__icon-button"
-              onClick={() => handleDelete(warehouse)}
-            >
-              <img src={deleteIcon} alt="Delete icon" />
-            </button>
-            <button className="warehouses__icon-button">
-              <img src={editIcon} alt="Edit icon" />
-            </button>
-          </div>
-        </div>
-      ))}
-      {showDeleteModal && (
-        <DeleteModal
-          onClose={handleCloseModal}
-          title={`Delete ${selectedWarehouse?.warehouse_name} warehouse?`}
-          message = {`Please confirm that you’d like to delete the  ${selectedWarehouse.warehouse_name} from the list of warehouses. You won’t be able to undo this action.`}
-          onDeleteConfirm={confirmDeleteWarehouse}
-        />
-      )}
-    </div>
-  );
+			{warehouses.map((warehouse) => (
+				<div key={warehouse.id} className="warehouses__item">
+					<div className="warehouses__text-box">
+						<div className="warehouses__column">
+							<div className="warehouses__content warehouses__content--long">
+								<h3 className="warehouses__mobile-header">Warehouse</h3>
+								<Link
+									to={`/warehouses/${warehouse.id}`}
+									className="warehouses__link"
+								>
+									<div className="warehouses__name-wrapper">
+										<p className="warehouses__name">
+											{warehouse.warehouse_name}
+										</p>
+										<img
+											src={arrowRight}
+											alt="Arrow Right"
+											className="warehouses__name--icon"
+										/>
+									</div>
+								</Link>
+							</div>
+							<div className="warehouses__content warehouses__content--long">
+								<h3 className="warehouses__mobile-header">Address</h3>
+								<p>{`${warehouse.address}, ${warehouse.city}, ${warehouse.country}`}</p>
+							</div>
+						</div>
+						<div className="warehouses__column">
+							<div className="warehouses__content warehouses__content--long">
+								<h3 className="warehouses__mobile-header">Contact Name</h3>
+								<p>{warehouse.contact_name}</p>
+							</div>
+							<div className="warehouses__content warehouses__content--long">
+								<h3 className="warehouses__mobile-header">
+									Contact Information
+								</h3>
+								<p>{warehouse.contact_phone}</p>
+								<p>{warehouse.contact_email}</p>
+							</div>
+						</div>
+					</div>
+					<div className="warehouses__action">
+						<h3 className="warehouses__mobile-header active">ACTIONS</h3>
+						<button
+							className="warehouses__icon-button"
+							onClick={() => handleDelete(warehouse)}
+						>
+							<img src={deleteIcon} alt="Delete icon" />
+						</button>
+						<NavLink to={`/warehouses/${warehouse.id}/edit`}>
+							<button className="warehouses__icon-button">
+								<img src={editIcon} alt="Edit icon" />
+							</button>
+						</NavLink>
+					</div>
+				</div>
+			))}
+			{showDeleteModal && (
+				<DeleteModal
+					onClose={handleCloseModal}
+					title={`Delete ${selectedWarehouse?.warehouse_name} warehouse?`}
+					message={`Please confirm that you’d like to delete the  ${selectedWarehouse.warehouse_name} from the list of warehouses. You won’t be able to undo this action.`}
+					onDeleteConfirm={confirmDeleteWarehouse}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default WarehouseList;
